c5cbedc62238271358a4854c97f78d44cd79cb3d
roslein <vernicagee953@gmail.com>
2025-06-28 23:32:29 +0300

    Add files via upload

---
+++ initials.py
@@ -0,0 +1,9 @@
+
+# im crazy
+print ('K     K      N        N')
+print ('K   K        N N      N')
+print ('K K          N   N    N')
+print ('KK           N    N   N')
+print ('K  K         N      N N')
+print ('K    K       N       NN ')
+print ('K      K     N        N')
